a78d04d5e15d2f704089cdc84afdb1fab1969922
edovinivan <79606635949@yandex.ru>
2021-10-08 10:47:31 +0300

    Added additional entities. Roles and UsersRoles

---
+++ src/main/java/com/example/cmtest/entities/Roles.java
@@ -0,0 +1,37 @@
+package com.example.cmtest.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "ROLES")
+public class Roles implements Serializable {
+    @Id
+    @Column(name = "ID")
+    Integer id;
+
+    @Column(name = "NAME", length = 100)
+    String name;
+
+    public Roles() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
